d29d382bf086710c5d869d66350560ccb4aac73a
unknown <rafolwen98@gmail.com>
2019-03-18 20:47:49 +0800

    oh so this is how you make an import from other js files

---
+++ my-app/src/MyInfo.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function MyInfo()
+{
+    let code = (
+    <div class="basic-info">
+        <h1> Sam Programmer </h1>
+        <p>
+            bluergh
+        </p>
+        <ul>
+            <li> i have put this inside a function </li>
+            <li> this is a </li>
+            <li> list </li>
+        </ul>
+    </div>
+    );
+    return code;
+}
+
+export default MyInfo;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,22 +1,4 @@
 import React    from "react";
 import ReactDOM from "react-dom";
-
-function MyInfo()
-{
-    let code = (
-    <div class="basic-info">
-        <h1> Sam Programmer </h1>
-        <p>
-            bluergh
-        </p>
-        <ul>
-            <li> i have put this inside a function </li>
-            <li> this is a </li>
-            <li> list </li>
-        </ul>
-    </div>
-    );
-    return code;
-}
-
+import MyInfo   from "./MyInfo.js";
 ReactDOM.render(<MyInfo />, document.getElementById("root"));
