f79a78614fd6f2b9354c38a34483f5a60ca8072d
unknown <rafolwen98@gmail.com>
2019-03-18 20:41:32 +0800

    able to import appriopriate dependencies, and use ReactDOM.render(*,*)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React    from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function MyInfo()
+{
+    let code = (
+    <div class="basic-info">
+        <h1> Sam Programmer </h1>
+        <p>
+            bluergh
+        </p>
+        <ul>
+            <li> i have put this inside a function </li>
+            <li> this is a </li>
+            <li> list </li>
+        </ul>
+    </div>
+    );
+    return code;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
